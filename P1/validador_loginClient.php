db1dec82d951894be964140dca0ce4abbba97464
JayleneCenicerosSevilla <55028366+JayleneCenicerosSevilla@users.noreply.github.com>
2020-02-10 14:22:23 -0600

    Add files via upload

---
+++ P1/validador_loginClient.php
@@ -0,0 +1,38 @@
+<script type="text/javascript" src="JavaScript/varios.js"></script>
+
+<?php
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+session_start(); //inicia sesion
+require "conexion.php";
+$con = conecta();
+
+$correo    = $_REQUEST['correo'];
+$pass = $_REQUEST['pass'];
+$clave = md5($pass);
+
+$sql = "SELECT *
+        FROM clientes
+        WHERE correo='$correo' AND pass='$pass' AND status=1 AND eliminado=0 ";
+
+$res = mysql_query($sql, $con);
+
+$num = numRegistros($res);
+
+if ($num == 0) {
+   echo "<script>
+           errorLogin();
+           location.href = 'loginClient.php';
+        </script>";
+	}
+	else {
+		$nombre    = mysql_result($res, 0, "nombre"); //cambie nombre de la variable para que coincidiera con la BD
+		$_SESSION["SesionUsuario"] = $nombre;
+
+echo "<script>
+
+        location.href = 'PrincipalClient.php?".SID."'; 
+      </script>";
+ 
+}
+?>
